0d1b8517007f449dca6aba81583d6d80ee1c1dc9
Isabela Oliveira <oliver.isabela2303@gmail.com>
2022-12-19 17:36:12 -0300

    teste - initial commit

---
+++ public/js/chart.js
@@ -0,0 +1,35 @@
+const barChart = document.getElementById('barChart'),
+    lineChart = document.getElementById('lineChart');
+
+new Chart(barChart, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+new Chart(lineChart, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1
+        }]
+    }
+});
